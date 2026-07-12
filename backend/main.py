e44af954af365acf3e2ed598a69d33dcb70e5663
sudeepa <sudeepm831@gmail.com>
2026-07-12 15:48:42 +0530

    Setup FastAPI backend

--- backend/main.py
+++ backend/main.py
@@ -1,25 +1,18 @@
 from fastapi import FastAPI
 
-from app.database.database import Base, engine
-from app.models import User
-
-Base.metadata.create_all(bind=engine)
-
 app = FastAPI(
     title="AI Resume Screening System",
     version="1.0.0"
 )
 
-
 @app.get("/")
 def home():
     return {
-        "message": "AI Resume Screening System API"
+        "message": "Welcome to AI Resume Screening System"
     }
-
 
 @app.get("/health")
 def health():
     return {
-        "status": "Running"
+        "status": "Server Running Successfully"
     }
